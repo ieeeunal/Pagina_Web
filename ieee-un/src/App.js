7c53af63f365b819ed0ba044fc3d3ebbe9ca9bc9
julianflancheros <jflancherosm@unal.edu.co>
2022-07-28 15:23:30 -0500

    Creation a helper file that contain all routes of the frontend project

--- ieee-un/src/App.js
+++ ieee-un/src/App.js
@@ -2,22 +2,32 @@
 import './Styles/app.sass'; 
 import './App.css';
 
+import routes from './Helpers/routes'
+
 import LandingPage from './Pages/LandingPage';
 import Chapter from './Pages/Chapter';
 import ChapterDetails from './Pages/ChapterDetails';
 import Login from './Pages/Login';
+import NotFound404 from './Pages/NotFound404';
 
 import { Routes, Route } from 'react-router-dom';
+import ChangePassword from './Pages/ChangePassword';
+import ValidateUser from './Pages/ValidateUser';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={ <LandingPage /> } />
-        <Route path="/sign-in" element={ <Login /> } />
-        <Route path="/chapter" element={ <Chapter /> } />
-        <Route path="/chapter/:id" element={ <ChapterDetails /> } />
-        <Route path="*" element={ <div> Error 404 </div> } />
+        <Route path={routes.home} element={ <LandingPage /> } />
+        <Route path={routes.login} element={ <Login /> } />
+        <Route path={routes.identidy} element={ <ValidateUser /> } />
+        <Route path={routes.changePassword} element={ <ChangePassword /> } />
+        <Route path={routes.chapter} element={ <Chapter /> } />
+        <Route
+          exact
+          strict
+          sensitive={false} path={routes.chapterId} element={ <ChapterDetails /> } /> 
+        <Route path={routes.notFound} element={ <NotFound404 /> } />
       </Routes>
     </>
   );
---
+++ ieee-un/src/Helpers/routes.js
@@ -0,0 +1,21 @@
+const routes= {
+// Paginas
+    home: "/",
+    login: "/sign-in",
+    identidy: "/valideIdentidy",
+    changePassword: "/recover",
+    chapter: "/chapter",
+    chapterId: "/chapter/:chapterId",
+    notFound: "*",
+
+// Secciones 
+    seccionQuienesSomos: "#quienes-somos",
+    seccionCapitulos: "#capitulos",
+    seccionEquipo: "#equipo",
+
+// Rutas Admin
+    // admin: "/Dashport",
+    // dashportHome: "/DashportHome",
+}
+
+export default routes;
