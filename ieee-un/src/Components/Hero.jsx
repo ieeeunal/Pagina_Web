c18425e29c316a4908df66ef6756f8a75a5d9256
julianflancheros <jflancherosm@unal.edu.co>
2022-07-28 15:51:06 -0500

    This component was converted to be dynamic using information fetched from the database.

--- ieee-un/src/Components/Hero.jsx
+++ ieee-un/src/Components/Hero.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
+import React from 'react'; 
 
-import '../Styles/LandingPage.sass';
-import Owl from '../Assets/Buho.svg';
-
-
-const Hero = ({children}) => {
+const Hero = ( props ) => {
+  let SecondSection = <section className={`section-2 ${props.chapterName}-bg-color-b`} ></section>;
   return (
     <>
-        <section className="section-1">
-            <div className="heroSection">
-                <div className="heroSection-text">
-                    {children}
-                </div>
-                <div className="">
-                    <img className="owl" src={Owl} alt="" />
-                </div>
-            </div>
-		</section>
+      <section className={`section-1 ${props.chapterName}-bg-color`}>
+        <div className="heroSection">
+          <div className="heroSection-text">
+            {props.children}
+            <div className="bg-img" style={{backgroundImage: `url(${props.backImage})`}}></div>
+          </div>
+        </div>
+      </section>
+      {props.secondColor === "Active" ? SecondSection : SecondSection = "0"}
     </>
   )
 }
