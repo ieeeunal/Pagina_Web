8c05b410e8c5f8af5537f7503ba6c822049938ac
julianflancheros <jflancherosm@unal.edu.co>
2022-07-28 15:36:02 -0500

    Upload fronted login

--- ieee-un/src/Pages/Login.jsx
+++ ieee-un/src/Pages/Login.jsx
@@ -1,35 +1,74 @@
-import React from 'react'
-import Hero from '../Components/Hero'
-// import { Link } from "react-router-dom"
+import { React, useState } from 'react'
 
-import '../Styles/Sign.sass';
+import styles from '../Styles/Sign.module.sass';
+
+import Hero from '../Components/Hero'
+import routes from '../Helpers/routes';
+
+import data from '../Components/utils/Chapters.json'
+import initial from "../Assets/chapters/AESS.png"
+
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-return (
-    <>
-        <Hero>
-            <form className='formSign-in'>
-                <h3>Sign In</h3>
-                <div className="mb-3">
-                    <label>Usuario</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-                <div className="mb-3">
-                    <label>Contraseña</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-                <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">
-                        Iniciar sesión
-                    </button>
-                </div>
-                <p className="forgot-password text-right">
-                    <a href="https://www.google.com" rel="noopener noreferrer">¿Olvidaste tu contraseña?</a>
-                </p>
-            </form>
+    let arrayChapters = []
+    let chapterSelect
+
+    const [image, setImage] = useState(initial);
+
+    data.map(item => {
+        if (!(item.name in arrayChapters)){
+            arrayChapters.push(item.name)
+        }
+    })
+
+    const selectOneImage = () => {
+        return (arrayChapters[Math.floor(Math.random() * arrayChapters.length)])
+    }
+    
+    const updateImage = () => {
+        const imageSelect = selectOneImage()
+        chapterSelect = imageSelect.toLowerCase()
+        import(`../Assets/chapters/${imageSelect}.png`).then( image => 
+            { 
+                setImage( image.default );
+            }
+            ).catch((e) => {
+                alert('Failed'); 
+            })
+    }
+
+    updateImage()
+    
+    return (
+        <>
+        <Hero chapterName={chapterSelect} backImage={ image }>
+            {/* <Hero chapterName="aess" color2="aess-color-b" backImage={ image }> */}
+        {/* backImage={require(filePath).default} */}
+        <form className={styles.formSignIn}>
+                    <h3>Inicia sesion</h3>
+                    <div className="mb-3">
+                        <label>Usuario</label>
+                        <input type="email" className="form-control" placeholder="Ingresa tu email" />
+                    </div>
+                    <div className="mb-3">
+                        <label>Contraseña</label>
+                        <input type="password" className="form-control" placeholder="Ingresa tu contraseña" />
+                    </div>
+                    <div className="d-grid">
+                        <button type="submit" className="btn btn-primary">
+                            Iniciar sesión
+                        </button>
+                    </div>
+                    <p className={`${styles.forgotPassword} text-right`}>
+                        <Link to={routes.identidy} rel="noopener noreferrer">¿Olvidaste tu contraseña?</Link>
+                    </p>
+                </form>
         </Hero>
-    </>
-)
+        </>
+    )
 }
 
 export default Login
+
+
