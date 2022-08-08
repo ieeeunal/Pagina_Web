97f8af2c66ece98dd287325952ad065c5c1965c7
julianflancheros <jflancherosm@unal.edu.co>
2022-08-08 18:19:36 -0500

    Update contect 404 page

--- ieee-un/src/Pages/NotFound404.jsx
+++ ieee-un/src/Pages/NotFound404.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+// Estilos
+import styles from "../Styles/404.module.sass"
+
+// Componentes
+import Hero from '../Components/Hero'
+import Media from '../Components/Media'
+
+// Rutas
+import routes from '../Helpers/routes'
+
+// Imagen 
+import imagenUse from "../Assets/chapters/AESS.png"
 
 const NotFound404 = () => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
+
   return (
-    <div>
-      <h3>PagenNot Found 404</h3>
-    </div>
+    <>
+      <Media />
+      <Hero chapterName="aess" color2="aess-color-b" secondColor="Active" backImage={imagenUse}>
+        <div class="col-md-6">
+          <h2 className="title mr-5">Error 404</h2>
+          <div className={styles.buttonText}>
+            <p> Ir al </p>
+              <Link to={routes.home} className="ml-4 ver-mas slide-bottom">Home</Link>
+          </div>
+        </div>
+      </Hero>
+    </>
   )
 }
 
