d44b514e7c215d4cd3216228542179f6986572a0
julianflancheros <jflancherosm@unal.edu.co>
2022-08-08 18:50:00 -0500

    Create footer like the proposed mockup

---
+++ ieee-un/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from '../Styles/Footer.module.sass'
+
+const Footer = () => {
+
+    return (
+        <>
+            <div className={styles.containerColor}>
+                <p>Hecho con &lt;3 por el equipo de IEEE UN </p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
