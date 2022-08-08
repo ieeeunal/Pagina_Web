a32d628b73ebc0a401d8162e307121a28e053aff
julianflancheros <jflancherosm@unal.edu.co>
2022-08-08 18:56:51 -0500

    Aggregate axions default route and delete files connection

--- ieee-un/src/index.js
+++ ieee-un/src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Axios from 'axios'
+
+
+// Axios.defaults.baseURL = ""; // Aqui va el enlace de la base de datos montada en nube
+Axios.defaults.baseURL = "http://localhost:4000";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    // <BrowserRouter basename="/Pagina_Web"> Linea de codigo para el despliege en github pages
-    <BrowserRouter> 
+    // <BrowserRouter basename="/Pagina_Web"> 
+    // Linea de codigo para el despliege en github pages */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
