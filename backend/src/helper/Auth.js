083062c1244f46373b6f9b15d032fd4ac8b82161
julianflancheros <jflancherosm@unal.edu.co>
2022-08-08 19:23:14 -0500

    Create a validation of credentials in backend

---
+++ backend/src/helper/Auth.js
@@ -0,0 +1,27 @@
+const Auth={}
+const jwt = require('jsonwebtoken');
+
+Auth.verificarToken = (req, res, next) =>{
+
+    if(!req.headers.autorizacion){
+        return res.json({
+            mensaje : 'No estas autorizado'
+        })
+    }
+
+    const token = req.headers.autorizacion;
+    if (token==='null'){
+        return res.json({
+            mensaje : 'No estas autorizado'
+        })
+    }
+
+    jwt.verify(token, 'Secreta', (error, resultados) => {
+        if (error) return res.json({
+            mensaje: 'No estas autorizado'
+        })
+        next();
+    })
+}
+
+module.exports = Auth
