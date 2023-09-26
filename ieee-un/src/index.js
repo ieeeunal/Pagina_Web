dfc293563325898b66d4ad02e29dd8496ab14ef3
julianflancheros <jflancherosm@unal.edu.co>
2023-09-25 22:40:29 -0500

    add comment code about the problem with the subsites

--- ieee-un/src/index.js
+++ ieee-un/src/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import Axios from 'axios'
 
 
-// Axios.defaults.baseURL = ""; // Aqui va el enlace de la base de datos montada en nube
+Axios.defaults.baseURL = ""; // Aqui va el enlace de la base de datos montada en nube
 Axios.defaults.baseURL = "http://localhost:4000";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     // <BrowserRouter basename="/Pagina_Web"> 
-    // Linea de codigo para el despliege en github pages */}
+    // Linea de codigo para el despliege en github pages 
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </React.StrictMode>
+  // </React.StrictMode> 
 );
 
+
+// import React from 'react'
+// import ReactDOM from "react-dom"
+// import App from "./App"
+// import { HashRouter } from "react-router-dom"
+
+
+// const rootElement = document.getElementById("root");
+
+// ReactDOM.render(
+
+//   <HashRouter>
+//       <App />
+//   </HashRouter>,
+  
+//   rootElement
+// );
