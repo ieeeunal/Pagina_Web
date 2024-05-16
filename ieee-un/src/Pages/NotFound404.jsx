deac5dad4de56e2b44e1f48552fa912443f71c21
julianflancheros <jflancherosm@unal.edu.co>
2024-05-16 01:42:24 -0500

    update some variables with dinamic data load in front and in the same way  use a color function for select the current color of the page

--- ieee-un/src/Pages/NotFound404.jsx
+++ ieee-un/src/Pages/NotFound404.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 // Estilos
@@ -11,18 +11,30 @@ import Media from '../Components/Media'
 // Rutas
 import routes from '../Helpers/routes'
 
-// Imagen 
-import imagenUse from "../Assets/chapters/AESS.png"
+
+
 
 const NotFound404 = () => {
+  const listColors = ["aess", "aps", "ras", "wie", "pes", "emc", "emb", "eds", "computer", "tems"]
+	const [color, setColor] = useState('aess');
+  const [image, setImage] = useState('');
   useEffect(() => {
     document.body.style.overflow = "hidden";
-  }, []);
+    let colorSelected = listColors[Math.floor(Math.random() * listColors.length)];
+    setColor(colorSelected);
+
+    // Imagen 
+    import(`../Assets/chapters/${colorSelected.toUpperCase()}.png`).then(image => {
+			setImage(image.default);
+    }).catch((e) => {
+      alert('Failed');
+    })
+  }, []); // Se ejecuta solo una vez al cargar la página
 
   return (
     <>
       <Media />
-      <Hero chapterName="aess" color2="aess-color-b" secondColor="Active" backImage={imagenUse}>
+      <Hero chapterName={color} color2={`${color}-color-b`} secondColor="Active" backImage={image}>
         <div class="col-md-6">
           <h2 className="title mr-5">Error 404</h2>
           <div className={styles.buttonText}>
