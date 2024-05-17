d772e2ded05621001e553eea4a6a9e03b075f120
julianflancheros <jflancherosm@unal.edu.co>
2024-05-17 01:40:18 -0500

    add new routes (Contact, Events, ZonaCentro, UNRobot) and load back to top componet over all pages

--- ieee-un/src/App.js
+++ ieee-un/src/App.js
@@ -4,7 +4,7 @@ import './Styles/app.sass';
 import routes from './Helpers/routes'
 
 import LandingPage from './Pages/LandingPage';
-import Chapter from './Pages/Chapter';
+import Chapter from './Pages/Chapters';
 import ChapterDetails from './Pages/ChapterDetails';
 import Login from './Pages/Login';
 import NotFound404 from './Pages/NotFound404';
@@ -18,16 +18,26 @@ import AdminUser from './Pages/AdminUser';
 import AdminMember from './Pages/AdminMember';
 import AdminChapter from './Pages/AdminChapter';
 import AdminMessage from './Pages/AdminMessage';
+import Contact from './Pages/Contact';
+import ScrollTop from './Components/ScrollTop';
+import Events from './Pages/Events';
+import ZonaCentro from './Pages/ZonaCentro';
+import UNRobot from './Pages/UNRobot';
 
 export default function App() {
   return (
     <>
+      <ScrollTop />
       <Routes>
         <Route path={routes.home} element={<LandingPage />} />
         <Route path={routes.login} element={<Login />} />
         <Route path={routes.identidy} element={<ValidateUser />} />
         <Route path={routes.changePassword} element={<ChangePassword />} />
         <Route path={routes.chapter} element={<Chapter />} />
+        <Route path={routes.contact} element={<Contact />} />
+        <Route path={routes.events} element={<Events />} />
+        <Route path={routes.zonaCentro} element={<ZonaCentro />} />
+        <Route path={routes.unRobot} element={<UNRobot />} />
         <Route
           exact
           strict
@@ -45,6 +55,7 @@ export default function App() {
         
         <Route path={routes.notFound} element={<NotFound404 />} />
       </Routes>
+      {/*<ScrollRestoration />*/}
     </>
   );
 }
