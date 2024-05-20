2bb68a989ba932e9cc0d8249a99ae5759f078412
julianflancheros <jflancherosm@unal.edu.co>
2024-05-19 20:31:48 -0500

    Format document with

--- ieee-un/src/Pages/LandingPage.jsx
+++ ieee-un/src/Pages/LandingPage.jsx
@@ -42,14 +42,14 @@ import ColorSelecter from '../Components/ColorSelecter';
 // const refMembers = db.ref(`members`); 
 // const refGeneral = db.ref(`general`); 
 
-export default function LandingPage () {
+export default function LandingPage() {
 	const listColors = ["aess", "aps", "ras", "wie", "pes", "emc", "emb", "eds", "computer", "tems", "zc"]
-	const [color, setColor ] = useState('aess');
+	const [color, setColor] = useState('aess');
 	const siteNavLinks = [
-        { navLinkName: 'Quienes Somos', scrollToId: routes.seccionQuienesSomos },
-        { navLinkName: 'Capítulos', scrollToId: routes.seccionCapitulos },
-        { navLinkName: 'Equipo', scrollToId: routes.seccionEquipo }
-    ];
+		{ navLinkName: 'Quienes Somos', scrollToId: routes.seccionQuienesSomos },
+		{ navLinkName: 'Capítulos', scrollToId: routes.seccionCapitulos },
+		{ navLinkName: 'Equipo', scrollToId: routes.seccionEquipo }
+	];
 
 	const changeColor = (id) => {
 		setColor(id);
@@ -62,130 +62,130 @@ export default function LandingPage () {
 		let colorSelected = listColors[Math.floor(Math.random() * listColors.length)];
 		setColor(colorSelected);
 	}, []);
-  
+
 	useEffect(() => {
 		document.body.style.overflow = "visible";
 	}, []);
-	
+
 	return (
 		<>
 			<Main colorBackground={color} siteNavLinks={siteNavLinks}>
-			<div id="inicio"></div>
-			<NavBar />
-			<Media />
-			<Hero backImage={Owl} chapterName="aess" color2="aess-color-b" secondColor="Active" >
-				<h2 className="title mr-5"> Ingenia tu camino </h2>
-				{/* <Link className="ml-4 ver-mas slide-bottom" to={routes.chapter}>Ver mas</Link> */}
-				{/* <a className="ml-4 ver-mas slide-bottom" href={pdfInfo}>Ver mas</a> */}
-				<a className="ml-4 ver-mas slide-bottom" href="https://drive.google.com/file/d/12vZ46vTwP1RkrBHCatfoaZt0Lbwy31l3/view?usp=sharing">Ver mas</a>
-								
-			</Hero>
-			{/* <Modal/> */}
+				<div id="inicio"></div>
+				<NavBar />
+				<Media />
+				<Hero backImage={Owl} chapterName="aess" color2="aess-color-b" secondColor="Active" >
+					<h2 className="title mr-5"> Ingenia tu camino </h2>
+					{/* <Link className="ml-4 ver-mas slide-bottom" to={routes.chapter}>Ver mas</Link> */}
+					{/* <a className="ml-4 ver-mas slide-bottom" href={pdfInfo}>Ver mas</a> */}
+					<a className="ml-4 ver-mas slide-bottom" href="https://drive.google.com/file/d/12vZ46vTwP1RkrBHCatfoaZt0Lbwy31l3/view?usp=sharing">Ver mas</a>
 
-			{/* <section className="section-3 mt-5  pt-5 full-width d-flex flex-column justify-content-center"> */}
-			<section className="section-3 full-width d-flex flex-column justify-content-center" id="quienes-somos" data-aos="fade-down"  data-aos-delay="500">
-				<div className="quienes-somos-container center">
-					<br />
-					<br />
-					<br />
-					<h1 className="title-container align-text-center"> ¿Quienes Somos? </h1>
+				</Hero>
+				{/* <Modal/> */}
 
-					<p className="">
-						La Rama Estudiantil IEEE de la Universidad Nacional de Colombia es un grupo estudiantil, asociado a
-						el Institute of Electrical and Electronics Engineers (IEEE), perteneciente a la región 9 comprendida
-						por Latinoamérica y el Caribe, e igualmente a Sección Colombia
-					</p>
+				{/* <section className="section-3 mt-5  pt-5 full-width d-flex flex-column justify-content-center"> */}
+				<section className="section-3 full-width d-flex flex-column justify-content-center" id="quienes-somos" data-aos="fade-down" data-aos-delay="500">
+					<div className="quienes-somos-container center">
+						<br />
+						<br />
+						<br />
+						<h1 className="title-container align-text-center"> ¿Quienes Somos? </h1>
 
-					<br />
-					<br />
+						<p className="">
+							La Rama Estudiantil IEEE de la Universidad Nacional de Colombia es un grupo estudiantil, asociado a
+							el Institute of Electrical and Electronics Engineers (IEEE), perteneciente a la región 9 comprendida
+							por Latinoamérica y el Caribe, e igualmente a Sección Colombia
+						</p>
 
-					<div className=""> </div>
-					<hr />
+						<br />
+						<br />
 
-					<div className="data-container row d-flex  justify-content-center align-items-center">
-						<div className="col d-flex justify-content-center" data-aos="fade-up" data-aos-delay="300">
-							<p className="a">
-								+260
-							</p>
-							<p className="b">
-								Miembros
-							</p>
-						</div>
+						<div className=""> </div>
+						<hr />
 
-						<div className="col d-flex justify-content-center" data-aos="fade-up" data-aos-delay="500">
-							<p className="a">
-								12
-							</p>
-							<p className="b">
-								Capítulos
-							</p>
-						</div>
+						<div className="data-container row d-flex  justify-content-center align-items-center">
+							<div className="col d-flex justify-content-center" data-aos="fade-up" data-aos-delay="300">
+								<p className="a">
+									+260
+								</p>
+								<p className="b">
+									Miembros
+								</p>
+							</div>
+
+							<div className="col d-flex justify-content-center" data-aos="fade-up" data-aos-delay="500">
+								<p className="a">
+									12
+								</p>
+								<p className="b">
+									Capítulos
+								</p>
+							</div>
 
 
-						<div className="col d-flex justify-content-center" data-aos="fade-up" data-aos-delay="700">
-							<p className="a">
-								32
-							</p>
-							<p className="b">
-								Proyectos
-							</p>
+							<div className="col d-flex justify-content-center" data-aos="fade-up" data-aos-delay="700">
+								<p className="a">
+									32
+								</p>
+								<p className="b">
+									Proyectos
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section>
 
-			{ /* Carousel */}
-			<section className={`position-relative full mt-5 transition-short ${color}-bg-color `} id="capitulos"> 
-				<Chapter changeColor={ changeColor } color={ color } />
-			</section>
+				{ /* Carousel */}
+				<section className={`position-relative full mt-5 transition-short ${color}-bg-color `} id="capitulos">
+					<Chapter changeColor={changeColor} color={color} />
+				</section>
 
-			{ /* Team */ }
-			<section className = "position-relative full-width pt-5" style = {{ height: 'fit-content'}}>
-				<div className = "team-div d-flex flex-column align-items-center mt-5" id="equipo">
-					<img className = "team-icon" src= { team } alt="" />
-					<div className = "row mt-3 align-items-center">
-						<div className = "col"><span className = "line-title" /></div>
-						<div className = "col team-title">Nuestro Equipo</div>
-						<div className = "col"><span className = "line-title" /></div>
-					</div>				
-				</div>
-				<div className = "w-100 position-relative" style = {{ height: 'fit-content'}}>
-					<div className = "arrow-up"> </div>
-					<div className = "card-container-team position-relative">
-						<CardsList /> 
+				{ /* Team */}
+				<section className="position-relative full-width pt-5" style={{ height: 'fit-content' }}>
+					<div className="team-div d-flex flex-column align-items-center mt-5" id="equipo">
+						<img className="team-icon" src={team} alt="" />
+						<div className="row mt-3 align-items-center">
+							<div className="col"><span className="line-title" /></div>
+							<div className="col team-title">Nuestro Equipo</div>
+							<div className="col"><span className="line-title" /></div>
+						</div>
 					</div>
-				</div>
-				<div className = "arrow-down"> </div>
-			</section> 
+					<div className="w-100 position-relative" style={{ height: 'fit-content' }}>
+						<div className="arrow-up"> </div>
+						<div className="card-container-team position-relative">
+							<CardsList />
+						</div>
+					</div>
+					<div className="arrow-down"> </div>
+				</section>
 
-			{/* WordCloudThingToLearn */}
-			<section className = "position-relative full-width" id="ieeestaCharla" data-aos="">
-				<div className = "team-div d-flex flex-column align-items-center mb-5" id="equipo">
-					<img className = "team-icon" src= { learn } alt="" />
-					<div className = "row mt-3 align-items-center">
-						<div className = "col"><span className = "line-title" /></div>
-						<div className = "col team-title">¿Qué temas la gente quiere aprender con nosotros?</div>
-						<div className = "col"><span className = "line-title" /></div>
-					</div>				
-				</div>	
-				<WordCloud /> 
-			</section>
+				{/* WordCloudThingToLearn */}
+				<section className="position-relative full-width" id="ieeestaCharla" data-aos="">
+					<div className="team-div d-flex flex-column align-items-center mb-5" id="equipo">
+						<img className="team-icon" src={learn} alt="" />
+						<div className="row mt-3 align-items-center">
+							<div className="col"><span className="line-title" /></div>
+							<div className="col team-title">¿Qué temas la gente quiere aprender con nosotros?</div>
+							<div className="col"><span className="line-title" /></div>
+						</div>
+					</div>
+					<WordCloud />
+				</section>
 
-			
-			{ /* Events */ }
-			<section className = "position-relative full-width" id="eventos" data-aos="zoom-in">
-			<div className = "team-div d-flex flex-column align-items-center">
-				<img className = "team-icon" src= { events } alt="" />
-				<div className="row mt-3 align-items-center mt-5">
-					<div className="col"><span className="line-title" /></div>
-					<div className="col team-title">Eventos</div>
-					<div className="col"><span className="line-title" /></div>
-				</div>
-				</div>		
-				
-				<Calendar></Calendar>
-			</section>
-			</Main>			
+
+				{ /* Events */}
+				<section className="position-relative full-width" id="eventos" data-aos="zoom-in">
+					<div className="team-div d-flex flex-column align-items-center">
+						<img className="team-icon" src={events} alt="" />
+						<div className="row mt-3 align-items-center mt-5">
+							<div className="col"><span className="line-title" /></div>
+							<div className="col team-title">Eventos</div>
+							<div className="col"><span className="line-title" /></div>
+						</div>
+					</div>
+
+					<Calendar></Calendar>
+				</section>
+			</Main>
 		</>
 	)
 }
