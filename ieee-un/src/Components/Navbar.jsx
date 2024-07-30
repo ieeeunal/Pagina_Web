17ac4941773c850085a0fece41770ceced90592a
julianflancheros <jflancherosm@unal.edu.co>
2024-07-29 22:45:00 -0500

    Update navbar with the variable information for the three types of users

--- ieee-un/src/Components/Navbar.jsx
+++ ieee-un/src/Components/Navbar.jsx
@@ -18,6 +18,7 @@ export default function NavBar(props) {
         if (sessionStorage.getItem('token') && sessionStorage.getItem('rolUser')) {
             setLogeo(true);
             setRol(sessionStorage.getItem('rolUser'));
+            console.log("El rol del usuario es: ", rol)
         }
 
         // if (["aess", "aps", "ras", "wie", "pes", "emc", "emb", "eds", "computer", "tems"].includes(dataPage)) {
@@ -31,14 +32,14 @@ export default function NavBar(props) {
 
     const salir = () => {
         sessionStorage.clear()
-        window.location.href = "/"
+        window.location.href = "/Pagina_Web"
     }
 
     // const navLinks = [
     //     { navLinkName: 'Inicio', scrollToId: routes.seccionInicio },
-    // 	{ navLinkName: 'Quienes Somos', scrollToId: routes.seccionQuienesSomos },
-    // 	{ navLinkName: 'Capítulos', scrollToId: routes.seccionCapitulos },
-    // 	{ navLinkName: 'Equipo', scrollToId: routes.seccionEquipo },
+    //     { navLinkName: 'Quienes Somos', scrollToId: routes.seccionQuienesSomos },
+    //     { navLinkName: 'Capítulos', scrollToId: routes.seccionCapitulos },
+    //     { navLinkName: 'Equipo', scrollToId: routes.seccionEquipo },
     //     { navLinkName: 'Comentarios', scrollToId: routes.seccionComentarios }
     // ];
 
@@ -65,7 +66,7 @@ export default function NavBar(props) {
 
     return (
         <>
-            <Navbar collapseOnSelect expand="md" className={`${props.chapterName}-bg-color section__navbar scroll-navbar`} id="navbar"   >
+            {/* <Navbar collapseOnSelect expand="md" className={`${props.chapterName}-bg-color section__navbar scroll-navbar`} id="navbar"   >
                 <Container>
                     <Navbar.Brand>
                         <Link to={routes.home}>
@@ -79,11 +80,6 @@ export default function NavBar(props) {
                             />{' '}
                         </Link>
                     </Navbar.Brand>
-                    {/* <Navbar.Brand href="#home">
-                        <a className="section__navbar-logo" rel="noopener noreferrer" href={routes.home}>
-                            <img id="logo" className="logo-small" src={logo} alt="logo IEEE-UN" />
-                        </a>
-                    </Navbar.Brand> */}
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav section__navbar-list">
                         <Nav className="me-auto section__navbar-items">
@@ -99,86 +95,82 @@ export default function NavBar(props) {
                                 })
                             }
 
-                            {/* <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-                            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-
-
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown> */}
-
                         </Nav>
-                        {/* <Nav className="section__navbar-items">
+                        <Nav className="section__navbar-items">
                             <a class="btn login" href={routes.login}>Iniciar Sesión</a>
-                        </Nav> */}
-                        {/* <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
-                        </Nav> */}
+                        </Nav> 
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar> */}
+
+            <Navbar collapseOnSelect expand="md" className={`${props.chapterName}-bg-color section__navbar scroll-navbar`} id="navbar"   >
+                <Container>
+                    <Navbar.Brand>
+                        <Link to={routes.home}>
+                            <img
+                                alt="logo IEEE-UN"
+                                src={logo}
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top logo-small"
+                                id="logo"
+                            />{' '}
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav section__navbar-list">
+                        <Nav className="me-auto section__navbar-items">
+                            {props.siteNavLinks.map(id => {
+                                return (
+                                    <Nav.Link className="section__navbar-item" href={id.scrollToId}>
+                                        {id.navLinkName}
+                                        <span></span>
+                                    </Nav.Link>
+                                )
+                            })}
+                        </Nav>
+                            {
+                                // rol === "Administrador" || rol === "Gestor" ? 
+                                rol === "Administrador" ?
+                                    <>
+                                        <Nav.Link href={routes.admin} className="section__navbar-item-varios">Dashport</Nav.Link>
+                                        <Nav.Link href={routes.home} className="section__navbar-item-varios" onClick={() => salir()}>Salir</Nav.Link>
+                                    </>
+                                :
+                                rol === "Gestor" ?
+                                    ""
+                                    :
+                                    <Nav className="section__navbar-items-varios">
+                                        <a class="btn login" href={routes.login}>Iniciar Sesión</a>
+                                    </Nav>
+                            }
+                        
+                            {/* {
+                                rol === "Administrador" || rol === "Gestor"
+                                    ?
+                                    rol === "Administrador" ?
+                                        <div className="section__navbar-list">
+                                            <ul className="section__navbar-items" id="supportedContent">
+                                                <NavLink to={routes.admin} className="nav-item nav-link">Dashport</NavLink>
+                                                <NavLink to={routes.home} className="nav-item nav-link" onClick={() => salir()}>Salir</NavLink>
+                                            </ul>
+                                        </div>
+                                        :
+                                        <div className="section__navbar-list">
+                                        </div>
+                                    : <div className="section__navbar-list">
+                                        <ul className="section__navbar-items" id="supportedContent">
+                                            <li className="section__navbar-item">
+                                                <Link rel="noopener noreferrer" to={routes.login}> Login </Link>
+                                                <span></span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                            } */}
+                        
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
-            {/* <nav className="section__navbar navbar-collapse" id="navbar">
-                <div className="section__navbar-container">
-                    <a className="section__navbar-logo" rel="noopener noreferrer" href={routes.home}>
-                        <img id="logo" className="logo-small" src={logo} alt="logo IEEE-UN" />
-                    </a>
-                <div>
-            {
-              rol === "Administrador" || rol === "Gestor"
-               ? 
-              rol === "Administrador" ? 
-              <div className="section__navbar-list">
-                <ul className="section__navbar-items" id="supportedContent">
-                    <NavLink to={routes.admin} className="nav-item nav-link">Dashport</NavLink>
-                    <NavLink to={routes.home} className="nav-item nav-link" onClick={()=>salir()}>Salir</NavLink>
-                </ul>
-              </div>
-              : 
-              <div className="section__navbar-list"> 
-              </div>
-            : <div className="section__navbar-list">
-                <ul className="section__navbar-items" id="supportedContent">
-                    {navLinks.map((id, index) => {  return ( <li key={index} className="section__navbar-item"> <a href={id.scrollToId} rel="noopener noreferrer"> {id.navLinkName} </a><span></span></li>)}) }
-                    <li className="section__navbar-item">
-                        <Link rel="noopener noreferrer" to={routes.login}> Login </Link>
-                        <span></span>
-                    </li>
-                </ul>
-                </div>
-            }
-            </div>
-                    {/* <div className="section__navbar-list">
-                        <ul className="section__navbar-items" id="supportedContent">
-                            {
-                                navLinks.map(id => {  
-                                    return (
-                                    <li className="section__navbar-item">
-                                        <a rel="noopener noreferrer" href={id.scrollToId}> {id.navLinkName} </a>
-                                        <span></span>
-                                    </li>
-                                    )
-                                }) 
-                            }
-                            <li className="section__navbar-item">
-                                <Link rel="noopener noreferrer" to={routes.login}> Login </Link>
-                                <span></span>
-                            </li>
-                        </ul>
-                    </div> 
-                </div>
-            </nav> */}
         </>
     )
 }
